ed12692c1ede083ff4ffbbb0e0f7a050a66986ed
augustozanellato <augustozanellato@users.noreply.github.com>
2016-11-08 12:24:53 +0100

    Commentato start.sh

--- Homebridge/ConfigPi1/start.sh
+++ Homebridge/ConfigPi1/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# Homebridge launcher
+# By Augusto Zanellato
+# Team ITI F. Severi
+# ExpoScuola 2016
+# Avvio il demone pigpiod che mi serve per controllare le porte GPIO e sospendo lo script per 3 secondi
 sudo pigpiod
 sleep 3
+# Imposto il range del PWM sulla GPIO16
 pigs PRS 16 100
-sleep 3
+# Avvio Homebridge in una sessione screen come root, specificando la cartella di configurazione
 screen -DmS homebridge sudo homebridge -I -D -U /home/*/.homebridge
