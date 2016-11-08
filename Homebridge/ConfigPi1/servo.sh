657f4c69a3d457bd86387ec2b1e19c22a1539ee0
augustozanellato <augustozanellato@users.noreply.github.com>
2016-11-08 12:34:31 +0100

    Commentato servo.sh

--- Homebridge/ConfigPi1/servo.sh
+++ Homebridge/ConfigPi1/servo.sh
@@ -4,20 +4,24 @@
 # Team ITI F. Severi
 # ExpoScuola 2016
 
+#Assegno i tre parametri dello script a tre diverse varibili
 rw=$1
 gpio=$2
 pulse=$3
 
+#Se il file di stato del servomotore selezionato non esiste lo creo
 if [ ! -f /tmp/$gpio.servo ]; then
 touch /tmp/$gpio.servo
 echo 0 > /tmp/$gpio.servo
 fi
 
+#Se il primo parametro è 'w' imposta la posizione del servo e la scrivo nel file di configurazione del servo selezionato
 if [ $rw == "w" ]; then
 pigs s $gpio $pulse
 status=$4
 echo $status > /tmp/$gpio.servo
 
+#Se il primo parametro è 'r' stampa la posizione attuale del servo
 elif [ $rw == "r" ]; then
 status=$(cat /tmp/$gpio.servo)
 if [ $status == "on" ]; then
